41a6e91a7a5f31d29c86a691417a7b0c0feb7f37
OGTutor <i.send.you.a.letter@gmail.com>
2023-04-27 02:56:45 +0300

    Added basic page components

---
+++ src/components/screens/about/About.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const About: FC = () => {
+	return <div>About</div>;
+};
+
+export default About;
---
+++ src/components/screens/skills/Skills.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Skills: FC = () => {
+	return <div>Skills</div>;
+};
+
+export default Skills;
