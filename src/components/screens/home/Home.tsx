02e78de70e383892790d7fb148197f1366ed1055
OGTutor <i.send.you.a.letter@gmail.com>
2023-04-28 00:11:21 +0300

    Added basic home screen

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,7 +1,29 @@
 import { FC } from 'react';
+import './Home.scss';
 
 const Home: FC = () => {
-	return <div>Home</div>;
+	return (
+		<div className="screen" id="Home">
+			<div className="main">
+				<header className="header__one">
+					<div className="header">
+						<div className="header__item">
+							<h2 className="title">Home page</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur,
+								adipisicing elit. Maiores unde nulla odio saepe,
+								eius provident.
+							</p>
+						</div>
+					</div>
+				</header>
+			</div>
+
+			<div className="main__background"></div>
+			<div className="shadow one"></div>
+			<div className="shadow two"></div>
+		</div>
+	);
 };
 
 export default Home;
