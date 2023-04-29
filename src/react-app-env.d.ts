fbff07d1da8490e76c62a035a80cb9ea1ca0a4c0
OGTutor <i.send.you.a.letter@gmail.com>
2023-04-29 17:38:55 +0300

    Fix react-app-env

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,3 +2,13 @@ declare module '*.mp4' {
 	const src: string;
 	export default src;
 }
+
+declare module '*.png' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.jpg' {
+	const src: string;
+	export default src;
+}
