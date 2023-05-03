3631edff9877b10fd43f2ca4ddbb87d702f59064
OGTutor <i.send.you.a.letter@gmail.com>
2023-05-03 22:44:10 +0300

    Added declare for gif n mp3 formats

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -12,3 +12,13 @@ declare module '*.jpg' {
 	const src: string;
 	export default src;
 }
+
+declare module '*.gif' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.mp3' {
+	const src: string;
+	export default src;
+}
